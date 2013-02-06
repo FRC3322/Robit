da9cd3d745413f836fd51609d65d9ca3194122fc
Kevin O'Brien <obrienkevinj2015@gmail.com>
2013-02-05 21:01:23 -0500

    adjusted shooter code

--- Commands/Shoot.cpp
+++ Commands/Shoot.cpp
@@ -21,7 +21,7 @@ Shoot::Shoot() {
 
 // Called just before this Command runs the first time
 void Shoot::Initialize() {
-	
+	SetTimeout(0.5);
 }
 
 // Called repeatedly when this Command is scheduled to run
@@ -33,7 +33,7 @@ void Shoot::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool Shoot::IsFinished() {
-	return false;
+	return IsTimedOut();
 }
 
 // Called once after isFinished returns true
