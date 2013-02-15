46b6eb020998655125e0653cbf9f76acfd6763c8
Adrian <AdrS1118@gmail.com>
2013-02-15 18:31:06 -0500

    solved a fix me in diagnostic.cpp

--- Diagnostics.cpp
+++ Diagnostics.cpp
@@ -53,38 +53,11 @@ void Diagnostics::Run() {
 }
 
 void Diagnostics::Snapshot(char *mode, double start, double end, double gyroAngle) {
-	Synchronized sync(m_writing);
-
-	if (m_buf_len < DIAG_SIZE - DIAG_LINE_SIZE) {
-		// The writer must guarantee never to use more than DIAG_LINE_SIZE bytes.
-		// If that amount of buffer space is not available, it means buffer flushing
-		// is lagging behind. Skip writing until the buffer flushes. TODO: it's
-		// not clear whether new snapshots are more valuable than old -- we could
-		// push older snapshots out of the buffer if the buffer is full.
-
-		int len = sprintf(m_buf + m_buf_len, "%s,%.8f,%.8f,%.2f\n", mode, start, end, gyroAngle);
-		m_buf_len += len;
-
-		if (m_buf_len > DIAG_SIZE / 2) {
-			// If the buffer is full enough, start flushing.
-			semGive(m_flushing);
-		}
-	}
+	this->bufferPrintf("%s,%.8f,%.8f,%.2f\n", mode, start, end, gyroAngle);
 }
 void Diagnostics::MotorSnapShot(unsigned int motorNumber, float busVoltage, float outputCurrent)
 {
-	//FIXME copied from Snapshot
-	Synchronized sync(m_writing);
-
-	if (m_buf_len < DIAG_SIZE - DIAG_LINE_SIZE) {
-		int len = sprintf(m_buf + m_buf_len, "m,%d,%.2f,%.2f\n", motorNumber, busVoltage, outputCurrent);
-		m_buf_len += len;
-		
-		if (m_buf_len > DIAG_SIZE / 2) {
-			// If the buffer is full enough, start flushing.
-			semGive(m_flushing);
-		}
-	}
+	this->bufferPrintf("m,%d,%.2f,%.2f\n", motorNumber, busVoltage, outputCurrent);
 }
 int Diagnostics::bufferPrintf(const char* format,...)
 {
